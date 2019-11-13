7d29c10954fe95980e2b9b7283718474c3f9628c
emilyrenee <emily.renee.hagood@gmail.com>
2019-11-13 09:50:20 -0500

    loop-control, step by three and foreach no if-else (steps 1, 2)

---
+++ loop-control.php
@@ -0,0 +1,45 @@
+<?php
+
+
+// create array with values between 0 and 100, every three steps
+$numbers = [];
+
+$count = 0;
+
+while($count <= 100) {
+	if ($count % 3 == 0) {
+		array_push($numbers, $count);
+	}
+	$count++;
+}
+
+function printText($text, $repetition) {
+	while ($repetition > 0) {
+		print_r($text);
+		$repetition--;
+	}
+}
+
+foreach($numbers as $number) {
+	$text = "";
+	$repetition = 1;
+
+	if ($number == 3) {
+		$text = "Three";
+		$repetition = 1;
+		printText($text, $repetition);
+	}
+
+	if ($number == 9) {
+		$text = "Nine";
+		$repetition = 3;
+		printText($text, $repetition);
+	}
+
+	if ($number == 15) {
+		$text = "Fifteen";
+		$repetition = 5;
+		printText($text, $repetition);
+	}
+};
+
