0407eac9a44b38b8e48f319ede1807c3e26d5d84
emilyrenee <emily.renee.hagood@gmail.com>
2019-11-13 10:36:15 -0500

    functions steps 1, 2

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+
+function add($foo, $bar) {
+    return $foo + $bar;
+}
+
+function subtract($foo, $bar) {
+    return $foo - $bar;
+}
+
+function multiply($foo, $bar) {
+    return $foo * $bar;
+}
+
+function divide($foo, $bar) {
+    return $foo / $bar;
+}
