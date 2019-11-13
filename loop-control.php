794f82a71169f3ddf8c7805bce7dd43024f6088f
emilyrenee <emily.renee.hagood@gmail.com>
2019-11-13 10:20:09 -0500

    step 4 - using for loop

--- loop-control.php
+++ loop-control.php
@@ -3,21 +3,22 @@
 $numbers = [];
 $count = 0;
 
-while($count <= 100) {
+while ($count <= 100) {
 	if ($count % 3 == 0) {
 		array_push($numbers, $count);
 	}
 	$count++;
 }
 
-function printText($text, $repetition) {
+function printText($text, $repetition)
+{
 	while ($repetition > 0) {
 		print_r($text . PHP_EOL);
 		$repetition--;
 	}
 }
 
-foreach($numbers as $number) {
+foreach ($numbers as $number) {
 	$repetition = 1;
 
 	if ($number == 3) {
@@ -44,7 +45,7 @@ foreach($numbers as $number) {
 		$text = 'First number';
 		printText($text, $repetition);
 	}
-	
+
 	if ($number % 10 == 0) {
 		$text = $number . " is a round number";
 		printText($text, $repetition);
@@ -52,3 +53,40 @@ foreach($numbers as $number) {
 		printText($text, $repetition);
 	}
 };
+
+for ($iteration = 0; $iteration < count($numbers); $iteration++) {
+	$number = $numbers[$iteration];
+	$repetition = 1;
+
+	if ($number == 3) {
+		$text = "Three";
+		printText($text, $repetition);
+	}
+
+	if ($number == 9) {
+		$text = "Nine";
+		$repetition = 3;
+		printText($text, $repetition);
+	}
+
+	if ($number == 15) {
+		$text = "Fifteen";
+		$repetition = 5;
+		printText($text, $repetition);
+	}
+
+	if ($number % 7 == 0) {
+		$numberFactorials = $number / 7;
+		$text = "Sevens are lucky, this number has " . $numberFactorials . ",";
+		printText($text, $repetition);
+		$text = 'First number';
+		printText($text, $repetition);
+	}
+
+	if ($number % 10 == 0) {
+		$text = $number . " is a round number";
+		printText($text, $repetition);
+		$text = 'Last number';
+		printText($text, $repetition);
+	}
+}
