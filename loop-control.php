afecb857653ff704de83c36fd81f0c02afaeee6b
emilyrenee <emily.renee.hagood@gmail.com>
2019-11-13 10:00:38 -0500

    loop-control step 3

--- loop-control.php
+++ loop-control.php
@@ -1,9 +1,6 @@
 <?php
 
-
-// create array with values between 0 and 100, every three steps
 $numbers = [];
-
 $count = 0;
 
 while($count <= 100) {
@@ -13,30 +10,15 @@ while($count <= 100) {
 	$count++;
 }
 
-function printText($text, $repetition) {
-	while ($repetition > 0) {
-		print_r($text);
-		$repetition--;
-	}
-}
-
 foreach($numbers as $number) {
-	if ($number == 3) {
-		$text = "Three";
-		$repetition = 1;
-		printText($text, $repetition);
+	if ($number % 7 == 0) {
+		$numberFactorials = $number / 7;
+		print_r("Sevens are lucky, this number has " . $numberFactorials . "," . PHP_EOL);
+		print_r('First number' . PHP_EOL);
 	}
-
-	if ($number == 9) {
-		$text = "Nine";
-		$repetition = 3;
-		printText($text, $repetition);
-	}
-
-	if ($number == 15) {
-		$text = "Fifteen";
-		$repetition = 5;
-		printText($text, $repetition);
+	
+	if ($number % 10 == 0) {
+		print_r($number . " is a round number" . PHP_EOL);
+		print_r('Last number' . PHP_EOL);
 	}
 };
-
