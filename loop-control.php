f9636ec5c2fdfe61ab7e2e533e8dd1bfe1767d53
emilyrenee <emily.renee.hagood@gmail.com>
2019-11-13 10:25:48 -0500

    setp 5 - while loop plus some refactoring

--- loop-control.php
+++ loop-control.php
@@ -18,7 +18,8 @@ function printText($text, $repetition)
 	}
 }
 
-foreach ($numbers as $number) {
+function handleNumber($number)
+{
 	$repetition = 1;
 
 	if ($number == 3) {
@@ -52,41 +53,19 @@ foreach ($numbers as $number) {
 		$text = 'Last number';
 		printText($text, $repetition);
 	}
+}
+
+foreach ($numbers as $number) {
+	handleNumber($number);
 };
 
 for ($iteration = 0; $iteration < count($numbers); $iteration++) {
 	$number = $numbers[$iteration];
-	$repetition = 1;
 
-	if ($number == 3) {
-		$text = "Three";
-		printText($text, $repetition);
-	}
+	handleNumber($number);
+}
 
-	if ($number == 9) {
-		$text = "Nine";
-		$repetition = 3;
-		printText($text, $repetition);
-	}
-
-	if ($number == 15) {
-		$text = "Fifteen";
-		$repetition = 5;
-		printText($text, $repetition);
-	}
-
-	if ($number % 7 == 0) {
-		$numberFactorials = $number / 7;
-		$text = "Sevens are lucky, this number has " . $numberFactorials . ",";
-		printText($text, $repetition);
-		$text = 'First number';
-		printText($text, $repetition);
-	}
-
-	if ($number % 10 == 0) {
-		$text = $number . " is a round number";
-		printText($text, $repetition);
-		$text = 'Last number';
-		printText($text, $repetition);
-	}
+while ($iteration > count($numbers)) {
+	handleNumber($numbers[$iteration]);
+	$iteration++;
 }
