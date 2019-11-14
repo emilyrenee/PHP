5136ede04eb31fbba34164bdb8cd267bdd04208c
emilyrenee <emily.renee.hagood@gmail.com>
2019-11-14 09:38:26 -0500

    rmv unused class method

--- advanced/PC.php
+++ advanced/PC.php
@@ -6,8 +6,5 @@ require_once("./Workstation.php");
 use Advanced\Workstation as Station;
 
 class PC extends Station {
-    public function getWorkstationName() {
-        parent::getWorkstationName();
-        return Workstation::getWorkstationComputer();
-    }
+    //
 }
